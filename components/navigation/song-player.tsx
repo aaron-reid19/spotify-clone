f40914dc636f4c15a09711a2a773050ab26e648b
Kaley <kaleyywood@gmail.com>
2026-02-08 18:58:58 -0700

    song-player skeleton

---
+++ components/navigation/song-player.tsx
@@ -0,0 +1,30 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+const deviceIcon = require('@/assets/images/device-icon.png');
+const playIcon = require('@/assets/images/play-icon.png');
+
+const SongPlayer = () => {
+    return (
+        <View style={styles.container}>
+            {/*Sombr Album Art*/}
+
+            {/*Artist Details*/}
+
+            {/*Icons*/}
+
+        </View>
+    );
+};
+
+// Styles
+const styles = StyleSheet.create({
+    container: {},
+    albumArt: {},
+    songInfo: {},
+    songTitle: {},
+    artistName: {},
+    deviceIcon: {},
+    playIcon: {},
+});
+
+export default SongPlayer;
